1b19625cdb50d9d58d975f1d4e36a8fdf99afd2e
JoshuaTyler-SmoothStack <Joshua.TylerSmoothStack@com>
2021-03-21 13:22:08 -0400

    hotfix: case sensitive time formatting

--- src/main/java/com/ss/utopia/timeformatting/FlightTimeFormatter.java
+++ src/main/java/com/ss/utopia/timeformatting/FlightTimeFormatter.java
@@ -6,7 +6,7 @@ public final class FlightTimeFormatter {
   
   private static DateTimeFormatter formatter;
 
-  private static final String YYYY_MM_DD_HH_MM_SS = "YYYY-MM-DD HH:MM:SS";
+  private static final String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
 
   private FlightTimeFormatter() {
     throw new IllegalStateException("Utilility class 'FlightTimeFormatter' is static and should not be instantiated.");
