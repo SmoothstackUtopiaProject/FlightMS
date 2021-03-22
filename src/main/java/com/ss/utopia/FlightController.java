1d784566b034452ea3982e9a67c0938395ce1577
SS-christianReyes <christian.reyes@smoothstack.com>
2021-03-22 00:30:00 -0400

    Update FlightController.java

--- src/main/java/com/ss/utopia/FlightController.java
+++ src/main/java/com/ss/utopia/FlightController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -152,6 +153,7 @@ public class FlightController {
 		);
 	}
 
+	
 	@ExceptionHandler(HttpMessageNotReadableException.class)
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ResponseEntity<Object> invalidMessage() {
